cd6fa01a65762c3d409fdfe6167cb83664e9f7d4
Wanff <kevinrowang@gmail.com>
2024-04-28 19:19:07 +0000

    prep for dutch

--- scripts/run_gcg.sh
+++ scripts/run_gcg.sh
@@ -13,7 +13,7 @@ python run_gcg.py \
     --model_name "llama2_7b" \
     --save_path "$SAVE_PATH" \
     --monitor_type "act" \
-    --probe_data_path "../data/llama2_7b/harmbench_alpaca_" \
+    --probe_data_path "/home/ubuntu/rowan/white-box-filtering/data/llama2_7b/jb_" \
     --probe_layer 24 \
     --monitor_loss_weight 1 \
     --file_spec "$FILE_SPEC" \
